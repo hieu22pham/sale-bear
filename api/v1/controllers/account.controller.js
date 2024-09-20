bf605a701b4a6a1c062ed97241b2a5275685ef75
hieu22pham <phammhieu2211@gmail.com>
2024-09-20 21:50:34 +0700

    update code

--- api/v1/controllers/account.controller.js
+++ api/v1/controllers/account.controller.js
@@ -1,1 +1,48 @@
 const Account = require("../models/account.model")
+const Role = require("../models/roles.model")
+const paginationHelper = require("../helpers/pagination")
+
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false
+  }
+
+  const countProduct = await Account.countDocuments(find)
+
+  let objectPagination = paginationHelper({
+    currentPage: 1,
+    limitItem: 4
+  }, req.query, countProduct
+  )
+
+  const records = await Account.find(find).select("-password -token").limit(objectPagination.limitItem)
+    .skip(objectPagination.skip)
+
+  if (records) {
+    for (const record of records) {
+      const role = await Role.findOne(
+        {
+          _id: record.role_id,
+          deleted: false
+        })
+
+      record.role = role
+    }
+
+    res.json({
+      code: 200,
+      message: "Lấy danh sách tài khoản thành công!",
+      data: records
+    })
+  } else {
+    res.json({
+      code: 400,
+      message: "Không tồn tại tài khoản nào!",
+    })
+  }
+
+}
+
+module.exports.create = async (req, res) => {
+
+}
--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,12 +1,14 @@
 const productRouter = require("./product.route")
 const errorRouter = require("./error404.route")
 const roleRouter = require("./role.route")
+const accountRouter = require('./account.route')
 
 module.exports = (app) => {
   const version = "/api/v1"
 
   app.use(version + '/products', productRouter)
   app.use(version + '/roles', roleRouter)
+  app.use(version + '/accounts', accountRouter)
   app.use(version + '/error404', errorRouter)
 
 }
